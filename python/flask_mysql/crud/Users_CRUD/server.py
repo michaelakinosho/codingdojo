d1ba507139f88ed9e95868c4df29cbdd63695038
Michael Akinosho <37967455+michaelakinosho@users.noreply.github.com>
2021-12-06 06:13:12 -0600

    Implementing CRUD for Flask application
    Working on implementing CRUD for Flask application

---
+++ python/flask_mysql/crud/Users_CRUD/server.py
@@ -0,0 +1,26 @@
+from logging import debug, error
+from flask import Flask, render_template, request, redirect, session# import the class from friend.py
+from user import User
+app = Flask(__name__)
+
+@app.route("/users")
+@app.route("/")
+def index():
+    # call the get all classmethod to get all friends
+    users = User.get_all()
+    print(users)
+    return render_template("index.html", all_users = users)
+
+@app.route('/create_friend',methods=["POST"])
+def create_friend():
+    data = {
+        "fname": request.form["fname"],
+        "lname": request.form["lname"],
+        "occ": request.form['occ']
+    }
+    
+    User.save(data)
+    return redirect('/')
+            
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ python/flask_mysql/db_connection/first_flask_mysql/server.py
@@ -0,0 +1,24 @@
+from logging import debug, error
+from flask import Flask, render_template, request, redirect, session# import the class from friend.py
+from friend import Friend
+app = Flask(__name__)
+@app.route("/")
+def index():
+    # call the get all classmethod to get all friends
+    friends = Friend.get_all()
+    print(friends)
+    return render_template("index.html", all_friends = friends)
+
+@app.route('/create_friend',methods=["POST"])
+def create_friend():
+    data = {
+        "fname": request.form["fname"],
+        "lname": request.form["lname"],
+        "occ": request.form['occ']
+    }
+    
+    Friend.save(data)
+    return redirect('/')
+            
+if __name__ == "__main__":
+    app.run(debug=True)
